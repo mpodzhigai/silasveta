62fd83cb7917aff62dba4c912ea8bd9fb2a4c92e
mpodzhigai <zxx.wm05@gmail.com>
2022-03-13 03:51:09 +0300

    now using recall as metric for skewed dataset optimized dataset loading

--- model.py
+++ model.py
@@ -2,39 +2,27 @@ import json
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import os
-import shutil
 
 
-def dataset_by_directory():
-
-    # Preparing data for keras dataset util
-
-    os.mkdir(os.path.join('data', 'test'))
-    os.mkdir(os.path.join('data', 'test', 'Cars'))
-    os.mkdir(os.path.join('data', 'test', 'Plants'))
-    os.mkdir(os.path.join('data', 'test', 'Others'))
-    os.mkdir(os.path.join('data', 'train'))
-    os.mkdir(os.path.join('data', 'train', 'Cars'))
-    os.mkdir(os.path.join('data', 'train', 'Plants'))
-    os.mkdir(os.path.join('data', 'train', 'Others'))
+def get_labels():
+    # Get labels from json and return them in alphanumeric order
     f = open('data.json', 'r')
     data = json.load(f)
     f.close()
-    for elem in data['initial_bundle']:
+    file_categories = {}
+    for elem in data['initial_bundle'] + data['test_bundle']:
         if elem['subcategory'] and elem['subcategory']['name'] and elem['subcategory']['name'] == 'Cars':
-            shutil.copyfile(elem['file'], os.path.join('data', 'train', 'Cars', elem['file'].split(os.sep)[-1]))
+            file_categories.update({elem['file'].split(os.sep)[-1]: 0})
         elif elem['category'] and elem['category']['name'] and elem['category']['name'] == 'Plants':
-            shutil.copyfile(elem['file'], os.path.join('data', 'train', 'Plants', elem['file'].split(os.sep)[-1]))
+            file_categories.update({elem['file'].split(os.sep)[-1]: 2})
         else:
-            shutil.copyfile(elem['file'], os.path.join('data', 'train', 'Others', elem['file'].split(os.sep)[-1]))
-    for elem in data['test_bundle']:
-        if elem['subcategory'] and elem['subcategory']['name'] and elem['subcategory']['name'] == 'Cars':
-            shutil.copyfile(elem['file'], os.path.join('data', 'test', 'Cars', elem['file'].split(os.sep)[-1]))
-        elif elem['category'] and elem['category']['name'] and elem['category']['name'] == 'Plants':
-            shutil.copyfile(elem['file'], os.path.join('data', 'test', 'Plants', elem['file'].split(os.sep)[-1]))
-        else:
-            shutil.copyfile(elem['file'], os.path.join('data', 'test', 'Others', elem['file'].split(os.sep)[-1]))
-    return
+            file_categories.update({elem['file'].split(os.sep)[-1]: 1})
+    labels = []
+    files = sorted(os.listdir('data'))
+    for name in files:
+        if name in file_categories.keys():
+            labels.append(file_categories[name])
+    return labels
 
 
 def lr_scheduler(epoch, lr):
@@ -51,9 +39,9 @@ def plot_dataset():
             plt.subplot(3, 3, i + 1)
             with tf.device('/cpu:0'):
                 plt.imshow(
-                        img_augmentation(images[i].numpy().astype("uint8"))
+                        img_augmentation(images[i]).numpy() / 255
                 )
-            plt.title(images_train.class_names[labels[i]])
+            plt.title(class_names[tf.math.argmax(labels[i])])
             plt.axis("off")
     plt.show()
 
@@ -82,8 +70,8 @@ def plot_predict():
         for i in range(9):
             plt.subplot(3, 3, i + 1)
             plt.imshow(images[i].numpy().astype("uint8"))
-            lbl = 'label: ' + images_test.class_names[labels[i]] + \
-                  ' predicted: ' + images_test.class_names[tf.math.argmax(model.predict(images)[i])]
+            lbl = 'label: ' + class_names[tf.math.argmax(labels[i])] + \
+                  ' predicted: ' + class_names[tf.math.argmax(model.predict(images)[i])]
             plt.title(lbl)
             plt.axis("off")
     plt.show()
@@ -140,27 +128,42 @@ x = tf.keras.layers.GlobalAveragePooling2D()(x)
 x = tf.keras.layers.Dropout(dropout_rate, name='top_dropout')(x)
 outputs = tf.keras.layers.Dense(3, activation='softmax')(x)
 model = tf.keras.Model(inputs, outputs)
+class_names = ['Cars', 'Others', 'Plants']
 
 # Training initiated if no previous weights found
 if not os.path.isdir('model'):
-    # Processing and loading the raw dataset
-    if not os.path.isdir(os.path.join('data', 'train')):
-        dataset_by_directory()
-    images_train = tf.keras.utils.image_dataset_from_directory(os.path.join('data', 'train'),
-                                                               image_size=(img_width, img_width))
-    images_test = tf.keras.utils.image_dataset_from_directory(os.path.join('data', 'test'),
-                                                              image_size=(img_width, img_width))
+    # Loading the data and labels
+    labels = get_labels()
+    images_train = tf.keras.utils.image_dataset_from_directory(os.path.dirname(__file__),
+                                                               labels=labels,
+                                                               shuffle=False,
+                                                               seed=0,
+                                                               validation_split=0.2,
+                                                               subset='training',
+                                                               image_size=(img_width, img_width),
+                                                               label_mode='categorical')
+    images_train.shuffle(3000, reshuffle_each_iteration=True)
+    images_test = tf.keras.utils.image_dataset_from_directory(os.path.dirname(__file__),
+                                                              labels=labels,
+                                                              shuffle=False,
+                                                              seed=0,
+                                                              validation_split=0.2,
+                                                              subset='validation',
+                                                              image_size=(img_width, img_width),
+                                                              label_mode='categorical')
     # plot_dataset()
 
     # Pass where we're training top layers of our NN from scratch
     lr_callback = tf.keras.callbacks.LearningRateScheduler(lr_scheduler)
     optimizer = tf.keras.optimizers.Adam(learning_rate=initial_lr)
-    model.compile(loss='sparse_categorical_crossentropy',
+    model.compile(loss='categorical_crossentropy',
                   optimizer=optimizer,
-                  metrics=['sparse_categorical_accuracy'])
+                  metrics=[tf.keras.metrics.Recall(class_id=0, name='recall_cars'),
+                           tf.keras.metrics.Recall(class_id=1, name='recall_others'),
+                           tf.keras.metrics.Recall(class_id=2, name='recall_plants')])
 
     early_stop = tf.keras.callbacks.EarlyStopping(
-        monitor='val_sparse_categorical_accuracy',
+        monitor='val_loss',
         min_delta=0.00005,
         patience=patience[0],
         restore_best_weights=True,
@@ -173,8 +176,8 @@ if not os.path.isdir('model'):
                         verbose=1,
                         callbacks=[lr_callback, early_stop])
 
-    acc = history.history['sparse_categorical_accuracy']
-    val_acc = history.history['val_sparse_categorical_accuracy']
+    acc = history.history['recall_plants']
+    val_acc = history.history['val_recall_plants']
 
     loss = history.history['loss']
     val_loss = history.history['val_loss']
@@ -184,13 +187,15 @@ if not os.path.isdir('model'):
 
     optimizer = tf.keras.optimizers.Adam(learning_rate=fine_tune_lr)
 
-    model.compile(loss='sparse_categorical_crossentropy',
+    model.compile(loss='categorical_crossentropy',
                   optimizer=optimizer,
-                  metrics=['sparse_categorical_accuracy'])
+                  metrics=[tf.keras.metrics.Recall(class_id=0, name='recall_cars'),
+                           tf.keras.metrics.Recall(class_id=1, name='recall_others'),
+                           tf.keras.metrics.Recall(class_id=2, name='recall_plants')])
 
     total_epochs = history.epoch[-1] + 1 + fine_tune_epochs
     early_stop = tf.keras.callbacks.EarlyStopping(
-        monitor='val_sparse_categorical_accuracy',
+        monitor='val_loss',
         min_delta=0.00005,
         patience=patience[1],
         restore_best_weights=True,
@@ -204,8 +209,8 @@ if not os.path.isdir('model'):
 
     model.save_weights(os.path.join('model', 'chkp'))
 
-    acc += history_fine.history['sparse_categorical_accuracy']
-    val_acc += history_fine.history['val_sparse_categorical_accuracy']
+    acc += history_fine.history['recall_plants']
+    val_acc += history_fine.history['val_recall_plants']
 
     loss += history_fine.history['loss']
     val_loss += history_fine.history['val_loss']
@@ -214,14 +219,10 @@ if not os.path.isdir('model'):
     print()
     print('Evaluating...')
     model.trainable = False
-    model.compile(loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'])
+    model.compile(loss='categorical_crossentropy', metrics=[tf.keras.metrics.Recall()])
     history_eval = model.evaluate(images_test)
     # plot_results()
     # plot_predict()
-
-    # Removing processed dataset
-    shutil.rmtree(os.path.join('data', 'test'))
-    shutil.rmtree(os.path.join('data', 'train'))
 
 # Load weights
 model.load_weights(os.path.join('model', 'chkp')).expect_partial()
